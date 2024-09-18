c4c981c8161fab9d51d5dd602f5fa5c2d64176f8
Enrique Alpízar <ealpizr@gmail.com>
2024-09-17 23:15:04 -0600

    Add `CorsLayers` to allow cross origin requests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use tower_http::cors::{Any, CorsLayer};
 use axum::{
-    response::IntoResponse, routing::get, Json, Router
+    http::{Method, StatusCode}, response::IntoResponse, routing::get, Json, Router
 };
 use shuttle_runtime::SecretStore;
 
@@ -7,7 +8,11 @@ use shuttle_runtime::SecretStore;
 async fn main(#[shuttle_runtime::Secrets] secrets: SecretStore) -> shuttle_axum::ShuttleAxum {
     let secret = secrets.get("SECRET").expect("SECRET not found");
     
-    let router = Router::new().route("/", get(hello_world));
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET])
+        .allow_origin(Any);
+
+    let router = Router::new().route("/", get(hello_world)).layer(cors);
     Ok(router.into())
 }
 
@@ -16,5 +21,5 @@ pub async fn hello_world() -> impl IntoResponse {
         "message": "Hello World!"
     });
 
-    Json(json_response)
+    (StatusCode::OK, Json(json_response))
 }
