b694cbc8702b72416719d8f68c291f32a950c9c1
Enrique Alpízar <ealpizr@gmail.com>
2024-09-17 22:11:06 -0600

    Install basic crates and setup a hello world endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    response::IntoResponse, routing::get, Json, Router
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+
+    println!("🚀 Server started successfully");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+pub async fn hello_world() -> impl IntoResponse {
+    let json_response = serde_json::json!({
+        "message": "Hello World!"
+    });
+
+    Json(json_response)
 }
