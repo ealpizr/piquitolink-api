d85e63c498897e504f28f16f0e0248eb7d213d92
Enrique Alpízar <ealpizr@gmail.com>
2024-09-17 22:36:00 -0600

    Setup shuttle.rs project and configure deployments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use axum::{
     response::IntoResponse, routing::get, Json, Router
 };
+use shuttle_runtime::SecretStore;
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(hello_world));
-
-    println!("🚀 Server started successfully");
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+#[shuttle_runtime::main]
+async fn main(#[shuttle_runtime::Secrets] secrets: SecretStore) -> shuttle_axum::ShuttleAxum {
+    let secret = secrets.get("SECRET").expect("SECRET not found");
+    
+    let router = Router::new().route("/", get(hello_world));
+    Ok(router.into())
 }
 
 pub async fn hello_world() -> impl IntoResponse {
